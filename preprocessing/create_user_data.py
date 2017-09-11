5041c7d048e4b7699899cedf70ea5f9874392b7c
githubbayes <2515687073@qq.com>
2017-09-11 23:36:05 +0800

    Update create_user_data.py

--- preprocessing/create_user_data.py
+++ preprocessing/create_user_data.py
@@ -5,17 +5,21 @@ import pandas as pd
 
 def parse_order(x):
     series = pd.Series()
-
+    '''将一个order里面的所有信息序列化'''
     series['products'] = '_'.join(x['product_id'].values.astype(str).tolist())
     series['reorders'] = '_'.join(x['reordered'].values.astype(str).tolist())
+    # 1 if this product has been ordered by this user in the past, 0 otherwise in the level of order products
     series['aisles'] = '_'.join(x['aisle_id'].values.astype(str).tolist())
     series['departments'] = '_'.join(x['department_id'].values.astype(str).tolist())
 
     series['order_number'] = x['order_number'].iloc[0]
+    # the order sequence number for this user
     series['order_dow'] = x['order_dow'].iloc[0]
+    # the day of the week the order was placed on
     series['order_hour'] = x['order_hour_of_day'].iloc[0]
+    # the hour of the day the order was placed on
     series['days_since_prior_order'] = x['days_since_prior_order'].iloc[0]
-
+    #days since the last order, capped at 30 (with NAs for order_number = 1)
     return series
 
 
@@ -25,8 +29,11 @@ def parse_user(x):
     series = pd.Series()
 
     series['order_ids'] = ' '.join(parsed_orders.index.map(str).tolist())
+    # one user bookings order id list
     series['order_numbers'] = ' '.join(parsed_orders['order_number'].map(str).tolist())
+    # one user order sequence
     series['order_dows'] = ' '.join(parsed_orders['order_dow'].map(str).tolist())
+    # the day of the week the order was placed on
     series['order_hours'] = ' '.join(parsed_orders['order_hour'].map(str).tolist())
     series['days_since_prior_orders'] = ' '.join(parsed_orders['days_since_prior_order'].map(str).tolist())
 
@@ -40,9 +47,9 @@ def parse_user(x):
     return series
 
 if __name__ == '__main__':
-    orders = pd.read_csv('~/Downloads/orders.csv')
-    prior_products = pd.read_csv('~/Downloads/order_products_prior.csv')
-    train_products = pd.read_csv('~/Downloads/order_products_train.csv')
+    orders = pd.read_csv('~/Downloads/orders.csv',nrows = 100000)
+    prior_products = pd.read_csv('~/Downloads/order_products_prior.csv',nrows = 100000)
+    train_products = pd.read_csv('~/Downloads/order_products_train.csv',nrows = 100000)
     order_products = pd.concat([prior_products, train_products], axis=0)
     products = pd.read_csv('~/Downloads/products.csv')
 
